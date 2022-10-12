1b3e7e19aaf6aa779b17657ee27132f7b93271ea
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-10-11 21:06:41 -0300

    chenge main banner

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -5,9 +5,9 @@ export default function MainBanner() {
   return (
     <div className='main-banner' style={{backgroundImage: "url('images/fermin-banner.JPG')"}}>
       <div className='shadow'>
-        <span className='subtitle'><p>Hello I'm</p></span>
+        <span className='subtitle'><p>Hola soy</p></span>
         <span className='title'><h1>Fermin Pacheco Zapiola</h1></span>
-        <span className='description'><p>and this is my rezume</p></span>
+        <span className='description'><p>y este es mi portfolio</p></span>
       </div>
     </div>
   )
